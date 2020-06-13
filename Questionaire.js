e5f60c8ded16bf09bb2c465331054422dcce5f26
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-06-13 15:55:46 +0930

    Added a separate JS file for Questions/Answers

---
+++ Questionaire.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        title: "Q1. JavaScript is a ___ -side programming language.",
+        options: ["Client", "Server", "Both", "None"],
+        answer: "Both"
+    
+    },
+    {
+        title: "Q2. How do you find the minimum of x and y using JavaScript?",
+        options: ["min(x,y)", "Math.min(x,y)", "Math.min(xy)", "min(xy)"],
+        answer: "Math.min(x,y)"
+    },
+    {
+        title: "Q3. Which of the following statements will throw an error?",
+        options: ["var fun = function bar( ){ }", "var fun = function bar{ }", "function fun( ){ }", "function( ){ }"],
+        answer: "var fun = function bar{ }"
+    },
+    {
+        title: "Q4. Which are the correct "if" statements to execute certain code if “x” is equal to 2?",
+        options: ["if(x 2)", "if(x = 2)", "if(x == 2)", "if(x != 2 )"],
+        answer: "if(x == 2)"
+    },
+    {
+        title: "Q5. Which of the following will write the message “Hello DataFlair!” in an alert box?",
+        options: ["alertBox(“Hello DataFlair!”)", "alert(Hello DataFlair!)", "msgAlert(“Hello DataFlair!”)", "alert(“Hello DataFlair!”)"],
+        answer: "alert(“Hello DataFlair!”)"
+    },
+    
+]
