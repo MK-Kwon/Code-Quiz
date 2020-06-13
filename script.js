b2ff36cb14478232dbbb003b6fab57e1a53994dd
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-06-13 23:33:38 +0930

    Added divs and header for the quiz and answer options

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ function init() {
         var score = 0;
         //When the quiz begins time remaining variable is assigned a value of 10 seconds per each question then will decrement by 1 seconds. If the user is wrong the time remaining will be deducted by 10.
         timeRemaining = numQuestions * 10;
-        timeRemainingEl.setAttribute("value",timeRemaining);
+        timeRemainingEl.setAttribute("value", timeRemaining);
 
-        var interval = setInterval(function() {
+        var interval = setInterval(function () {
             if (timeRemaining < 1) {
                 clearInterval(interval);
                 // if the timer reaches zero or the last question is answered, the quiz container disappears and final container appears.
@@ -32,9 +32,44 @@ function init() {
                 finalContainerEl.setAttribute("class", "container");
                 return;
             }
+            // timer decrements by 1 seconds (1000 milliseconds)
             timeRemaining = timeRemaining - 1;
-            timeRemainingEl.setAttribute("value",timeRemaining);
-        },1000);
+            timeRemainingEl.setAttribute("value", timeRemaining);
+        }, 1000);
+
+        function createQuiz(questionNum) {
+            // Once the quiz session begins, the header has the question and the answer buttons have a correct answer for the question.
+            quizContainerEl.innerHTML = "";
+            rowEl = document.createElement("div");
+            rowEl.setAttribute("class", "row");
+            quizContainerEl.append(rowEl);
+
+            colEl = document.createElement("div");
+            colEl.setAttribute("class", "col-0 col-sm-2");
+            rowEl.append(colEl);
+
+            colEl = document.createElement("div");
+            colEl.setAttribute("class", "col-12 col-sm-8");
+            rowEl.append(colEl);
+
+            colEl = document.createElement("div");
+            colEl.setAttribute("class", "col-0 col-sm-2");
+            rowEl.append(colEl);
+
+            colEl = rowEl.children[1];
+            rowEl = document.createElement("div");
+            rowEl.setAttribute("class", "row mb-3");
+            colEl.append(rowEl);
+
+            colEl = document.createElement("div");
+            colEl.setAttribute("class", "col-12");
+            rowEl.append(colEl);
+
+            headerEl = document.createElement("h2");
+            headerEl.innerHTML = questions[questionNum - 1].title;
+            colEl.append(headerEl);
+
+        }
 
 
     }
