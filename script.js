922ffe3573e4743b59ccda2f1586ac4e44f16668
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-06-13 20:36:53 +0930

    Started with first function. Initialpage setup, timer setup

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function init() {
-    var timeRemaining=0;
+    //Initial value of time remaining
+    var timeRemaining = 0;
 
     var startBtnEl = document.getElementById("start-button");
     var timeRemainingEl = document.getElementById("time-remaining");
@@ -12,5 +13,19 @@ function init() {
     var highscoreBtnEl = document.getElementById("highscore-container");
     var highScores = [];
 
-    
+    function startQuiz() {
+        //Upon starting the quiz the landing page disappears
+        landingContainerEl.setAttribute("class", "container empty");
+        //Quiz page appears
+        quizContainerEl.setAttribute("class", "container");
+        var currentQuestions = 1;
+        var score = 0;
+        //When the quiz begins time remaining variable is assigned a value of 10 seconds per each question then will decrement by 1 seconds. If the user is wrong the time remaining will be deducted by 10.
+        timeRemaining = numQuestions * 10;
+        timeRemainingEl.setAttribute("vale",timeRemaining);
+        
+
+    }
+
+
 }
